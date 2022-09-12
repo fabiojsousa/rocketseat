eff1448bdb1098d200892bef5f3807f84bc6d334
Fabio <fjasousa2018@gmail.com>
2022-09-12 14:17:23 -0300

    first commit

---
+++ nlw5-node/src/controllers/MessagesController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+import { MessageService } from '../services/MessageService';
+
+class MessagesController {
+  async create(request: Request, response: Response): Promise<Response> {
+    const { admin_id, text, user_id } = request.body;
+
+    const messageService = new MessageService();
+
+    const message = await messageService.create({
+      admin_id,
+      text,
+      user_id,
+    });
+
+    return response.json(message);
+  }
+
+  async showByUser(request: Request, response: Response) {
+    const { id } = request.params;
+
+    const messagesService = new MessageService();
+
+    const list = await messagesService.listByUser(id);
+
+    return response.json(list);
+  }
+}
+
+export { MessagesController };
