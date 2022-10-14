fa1d635391995dbe458e208b7a48305136c963a1
Fabio <fjasousa2018@gmail.com>
2022-10-14 11:43:55 -0300

    finished all components

--- ignite-lab/lab-ds/tailwind.config.cjs
+++ ignite-lab/lab-ds/tailwind.config.cjs
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.tsx'
-  ],
+  content: ['./src/**/*.tsx'],
   theme: {
     fontSize: {
       xs: 14,
@@ -10,23 +8,32 @@ module.exports = {
       md: 18,
       lg: 20,
       xl: 24,
-      '2xl': 32
+      '2xl': 32,
     },
     colors: {
-      'gray-900': '#121214',
-      'gray-800': '#202024',
-      'gray-400': '#7c7c8a',
-      'gray-200': '#c4c4cc',
-      'gray-100': '#e1e1e6',
+      transparent: 'transparent',
+      
+      black: '#000',
+      white: '#fff',
 
-      'cyan-500': '#81d8f7',
-      'cyan-300': '#9BE1FB',
+      gray: {
+        900: '#121214',
+        800: '#202024',
+        400: '#7c7c8a',
+        200: '#c4c4cc',
+        100: '#e1e1e6',
+      },
+
+      cyan: {
+        500: '#81d8f7',
+        300: '#9BE1FB',
+      },
     },
     extend: {
       fontFamily: {
-        sans: 'Inter, sans-serif'
-      }
+        sans: 'Inter, sans-serif',
+      },
     },
   },
   plugins: [],
-}
+};
